7e9bf6624b7502d6b629fab9a41d18929c509f45
Jake Westerberg <jakewesterberg@gmail.com>
2022-07-18 18:19:33 -0500

    pull_spks edit
    added new methods for pulling spikes to see if that resolves artifacting issue in averages.

--- pull_spks.m
+++ pull_spks.m
@@ -10,6 +10,8 @@ spks.pre_dur = 1;
 spks.on_dur = 1;
 spks.off_dur = 1;
 
+mthd = 'convolve_then_pull';
+
 varStrInd = find(cellfun(@ischar,varargin));
 for iv = 1:length(varStrInd)
     switch varargin{varStrInd(iv)}
@@ -21,52 +23,73 @@ for iv = 1:length(varStrInd)
             spks.on_dur = varargin{varStrInd(iv)+1};
         case {'-offd', 'off_dur'}
             spks.off_dur = varargin{varStrInd(iv)+1};
+        case {'-m', 'mthd'}
+            mthd = varargin{varStrInd(iv)+1};    
     end
 end
 
 %% Create GLO trial data
-for i = 1 : ss.total_trials
-    pre_spks{i}         = unit_info.spk_times(unit_info.spk_times>ss.on(i)-spks.pre_dur & unit_info.spk_times<=ss.on(i)) - ss.on(i);
-    pre_spkids{i}       = unit_info.spk_unit(unit_info.spk_times>ss.on(i) -spks.pre_dur & unit_info.spk_times<=ss.on(i));
-    on_spks{i}          = unit_info.spk_times(unit_info.spk_times>ss.on(i) & unit_info.spk_times<=ss.on(i)+spks.on_dur) - ss.on(i);
-    on_spkids{i}        = unit_info.spk_unit(unit_info.spk_times>ss.on(i) & unit_info.spk_times<=ss.on(i)+spks.on_dur);
+switch mthd
+    case 'convolve_then_pull'
 
-    % this current version corrects and aligns to off responses. For direct
-    % comparisons, it is important to consider this is 'stiched' rather
-    % than continuous. This should not be a worry for pre to on where they
-    % use the same alignment point, it is only a concern for on to off
-    off_spks{i}         = unit_info.spk_times(unit_info.spk_times>ss.off(i) & unit_info.spk_times<=ss.off(i)+spks.off_dur) - ss.off(i);
-    off_spkids{i}       = unit_info.spk_unit(unit_info.spk_times>ss.off(i) & unit_info.spk_times<=ss.off(i)+spks.off_dur);
+        conv_data                               = zeros(unit_info.total, max(unit_info.spk_times(:) + 1000));
+        conv_data(unit_info.spk_unit, ...
+            ceil(unit_info.spk_times * 1000))   = 1;
+        conv_data                               = single(conv_data);
+        conv_data                               = spks_conv(conv_data, spks_kernel('psp')) .* 1000;
+
+        spks.conv                               = nan(unit_info.total, ...
+                                                    ceil(spks.fs*(spks.pre_dur+spks.on_dur+spks.off_dur)), ...
+                                                    ss.total_trials);
+        
+        for i = 1 : ss.total_trials
+            spks.cnv(:,:,i)                     = [conv_data(:, (ss.on(i)-spks.predur)*1000 : (ss.on(i)+spks.on_dur)*1000), ...
+                                                    conv_data(:, ss.off(i)*1000 : (ss.off(i)+spks.off_dur)*1000)];
+        end
+
+
+    case 'pull_then_convolve'
+        for i = 1 : ss.total_trials
+            pre_spks{i}         = unit_info.spk_times(unit_info.spk_times>ss.on(i)-spks.pre_dur & unit_info.spk_times<=ss.on(i)) - ss.on(i);
+            pre_spkids{i}       = unit_info.spk_unit(unit_info.spk_times>ss.on(i) -spks.pre_dur & unit_info.spk_times<=ss.on(i));
+            on_spks{i}          = unit_info.spk_times(unit_info.spk_times>ss.on(i) & unit_info.spk_times<=ss.on(i)+spks.on_dur) - ss.on(i);
+            on_spkids{i}        = unit_info.spk_unit(unit_info.spk_times>ss.on(i) & unit_info.spk_times<=ss.on(i)+spks.on_dur);
+
+            % this current version corrects and aligns to off responses. For direct
+            % comparisons, it is important to consider this is 'stiched' rather
+            % than continuous. This should not be a worry for pre to on where they
+            % use the same alignment point, it is only a concern for on to off
+            off_spks{i}         = unit_info.spk_times(unit_info.spk_times>ss.off(i) & unit_info.spk_times<=ss.off(i)+spks.off_dur) - ss.off(i);
+            off_spkids{i}       = unit_info.spk_unit(unit_info.spk_times>ss.off(i) & unit_info.spk_times<=ss.off(i)+spks.off_dur);
+        end
+
+        spks.conv = [];
+
+        pre_spks_conv = single(nan(unit_info.total, spks.fs*spks.pre_dur, ss.total_trials));
+        on_spks_conv = single(nan(unit_info.total, spks.fs*spks.on_dur, ss.total_trials));
+        off_spks_conv = single(nan(unit_info.total, spks.fs*spks.off_dur, ss.total_trials));
+        for i = 1:unit_info.total
+            for j = 1:ss.total_trials
+                t_vec_pre = zeros(1,1000*spks.pre_dur);
+                t_vec_on = zeros(1,1000*spks.on_dur);
+                t_vec_off = zeros(1,1000*spks.off_dur);
+
+                t_vec_pre(ceil(pre_spks{j}(pre_spkids{j}==i)*1000 + spks.pre_dur*1000)) = 1;
+                t_vec_on(ceil(on_spks{j}(on_spkids{j}==i)*1000)) = 1;
+                t_vec_off(ceil(off_spks{j}(off_spkids{j}==i)*1000)) = 1;
+
+                t_vec_pre = spks_conv(t_vec_pre, spks_kernel('psp'));
+                t_vec_on = spks_conv(t_vec_on, spks_kernel('psp'));
+                t_vec_off = spks_conv(t_vec_off, spks_kernel('psp'));
+
+                pre_spks_conv(i,:,j) = single(t_vec_pre(1:(1000/spks.fs):end) .* 1000);
+                on_spks_conv(i,:,j) = single(t_vec_on(1:(1000/spks.fs):end) .* 1000);
+                off_spks_conv(i,:,j) = single(t_vec_off(1:(1000/spks.fs):end) .* 1000);
+            end
+        end
+
+        spks.conv = cat(2,spks.conv, pre_spks_conv); clear pre_spks_conv;
+        spks.conv = cat(2,spks.conv, on_spks_conv); clear on_spks_conv;
+        spks.conv = cat(2,spks.conv, off_spks_conv); clear off_spk_conv;
 end
-
-spks.conv = [];
-
-pre_spks_conv = single(nan(unit_info.total, spks.fs*spks.pre_dur, ss.total_trials));
-on_spks_conv = single(nan(unit_info.total, spks.fs*spks.on_dur, ss.total_trials));
-
-off_spks_conv = single(nan(unit_info.total, spks.fs*spks.off_dur, ss.total_trials));
-for i = 1:unit_info.total
-    for j = 1:ss.total_trials
-        t_vec_pre = zeros(1,1000*spks.pre_dur);
-        t_vec_on = zeros(1,1000*spks.on_dur);
-        t_vec_off = zeros(1,1000*spks.off_dur);
-
-        t_vec_pre(ceil(pre_spks{j}(pre_spkids{j}==i)*1000 + spks.pre_dur*1000)) = 1;
-        t_vec_on(ceil(on_spks{j}(on_spkids{j}==i)*1000)) = 1;
-        t_vec_off(ceil(off_spks{j}(off_spkids{j}==i)*1000)) = 1;
-
-        t_vec_pre = spks_conv(t_vec_pre, spks_kernel('psp'));
-        t_vec_on = spks_conv(t_vec_on, spks_kernel('psp'));
-        t_vec_off = spks_conv(t_vec_off, spks_kernel('psp'));
-
-        pre_spks_conv(i,:,j) = single(t_vec_pre(1:(1000/spks.fs):end) .* 1000);
-        on_spks_conv(i,:,j) = single(t_vec_on(1:(1000/spks.fs):end) .* 1000);
-        off_spks_conv(i,:,j) = single(t_vec_off(1:(1000/spks.fs):end) .* 1000);
-    end
-end
-
-spks.conv = cat(2,spks.conv, pre_spks_conv); clear pre_spks_conv;
-spks.conv = cat(2,spks.conv, on_spks_conv); clear on_spks_conv;
-spks.conv = cat(2,spks.conv, off_spks_conv); clear off_spk_conv;
-
 end
